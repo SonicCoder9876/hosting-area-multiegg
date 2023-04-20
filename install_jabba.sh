ae29eac744427d7ca1c4366b902998f182d81b36
SonicCoder9876 <soniccoder2780@gmail.com>
2023-04-20 20:34:44 +0200

    changed install path

--- install_jabba.sh
+++ install_jabba.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -sL https://github.com/shyiko/jabba/raw/master/install.sh | bash && . ~/.jabba/jabba.sh
+curl -sL https://github.com/shyiko/jabba/raw/master/install.sh | bash && . /home/container/.jabba/jabba.sh
